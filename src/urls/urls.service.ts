0c614d273ffdd88f49112812c7ad667c5c73c145
Mayara Milaneesi <mayaramilanesim@gmail.com>
2025-07-14 19:23:19 -0300

    feat(urls): implement shorten logic with unique code generation

--- src/urls/urls.service.ts
+++ src/urls/urls.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Url } from './entities/url.entity';
+import { customAlphabet } from 'nanoid';
+
+const nanoid = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  6,
+);
 
 @Injectable()
-export class UrlsService {}
+export class UrlsService {
+  constructor(
+    @InjectRepository(Url)
+    private readonly urlRepo: Repository<Url>,
+  ) {}
+
+  private async genUniqueCode(): Promise<string> {
+    let code: string;
+    let exists: Url | null;
+    do {
+      code = nanoid();
+      exists = await this.urlRepo.findOne({ where: { code } });
+    } while (exists);
+    return code;
+  }
+
+  async shorten(targetUrl: string): Promise<Url> {
+    const code = await this.genUniqueCode();
+    const url = this.urlRepo.create({ code, targetUrl });
+    return this.urlRepo.save(url);
+  }
+}
