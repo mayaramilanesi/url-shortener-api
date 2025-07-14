a16c6dd55c0e0505bafaeccbedfd654b9eacb1d6
Mayara Milaneesi <mayaramilanesim@gmail.com>
2025-07-14 18:32:10 -0300

    feat: register Url entity in UrlsModule

--- src/urls/urls.module.ts
+++ src/urls/urls.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Url } from './entities/url.entity';
 import { UrlsService } from './urls.service';
 import { UrlsController } from './urls.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Url])],
   providers: [UrlsService],
-  controllers: [UrlsController]
+  controllers: [UrlsController],
 })
 export class UrlsModule {}
