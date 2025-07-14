a77fd54285e298c991a9d8e22ada6d3599053b93
Mayara Milaneesi <mayaramilanesim@gmail.com>
2025-07-14 18:12:36 -0300

    feat: generate Urls module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UrlsModule } from './urls/urls.module';
+import { UrlsModule } from './urls/urls.module';
 
 @Module({
   imports: [
