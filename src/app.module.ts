5e0f944db160126479972fb0c96b7b10a2a6019e
Mayara Milaneesi <mayaramilanesim@gmail.com>
2025-07-14 18:04:40 -0300

    feat: configure TypeORM in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UrlsModule } from './urls/urls.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UrlsModule,
+  ],
 })
 export class AppModule {}
