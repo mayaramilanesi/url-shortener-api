7f7541dd4fa5f29254f3662de7c4eb0ae5d09c90
Mayara Milaneesi <mayaramilanesim@gmail.com>
2025-07-15 18:11:51 -0300

    chore: import UsersModule, AuthModule and enable ConfigModule globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UrlsModule } from './urls/urls.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
     }),
+
     UrlsModule,
     UsersModule,
     AuthModule,
