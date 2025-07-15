55b234817aeb5a8cf6fff78d7b63c4bacdf4ecc9
Mayara Milaneesi <mayaramilanesim@gmail.com>
2025-07-15 04:53:42 -0300

    feat(urls): add GET /:code endpoint for redirect and click counting

--- src/urls/urls.controller.ts
+++ src/urls/urls.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { UrlsService } from './urls.service';
 import { CreateUrlDto } from './dto/create-url.dto';
+import { Response } from 'express';
 
 @Controller()
 export class UrlsController {
@@ -10,5 +11,14 @@ export class UrlsController {
   async shorten(@Body() dto: CreateUrlDto) {
     const shortUrl = await this.urlsService.shortenUrl(dto.url);
     return { shortUrl };
+  }
+
+  @Get(':code')
+  async handleRedirect(
+    @Param('code') code: string,
+    @Res() res: Response,
+  ): Promise<void> {
+    const url = await this.urlsService.getAndCountUrlByCode(code);
+    res.redirect(url.targetUrl);
   }
 }
