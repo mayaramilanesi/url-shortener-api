ec915353dc07c20fb398d8aacb339b38f742655d
Mayara Milaneesi <mayaramilanesim@gmail.com>
2025-07-15 16:29:36 -0300

    feat(users): implement create user with bcrypt and prevent duplicate emails

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepo: Repository<User>,
+  ) {}
+
+  async create(email: string, password: string): Promise<User> {
+    const exists = await this.usersRepo.findOne({ where: { email } });
+    if (exists) throw new ConflictException('Email already in use');
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = this.usersRepo.create({ email, passwordHash });
+    return this.usersRepo.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.usersRepo.findOne({ where: { email, deletedAt: undefined } });
+  }
+}
